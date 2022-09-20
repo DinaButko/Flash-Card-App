03f6c7f28ecfc7b455d19c461ebd6a8a31c3aaa1
DzinaButko <butkodina@gmail.com>
2022-09-20 09:36:59 -0400

    added dict with words

--- Flash Card App/main.py
+++ Flash Card App/main.py
@@ -1,13 +1,17 @@
 from tkinter import *
 import json
 import time
+import random
+
+import pandas
+import pandas as pd
 
 # ----------CONSTANTS-------------#
 BACKGROUND_COLOR = "#B1DDC6"
 
-SECONDS = 3
-timer = None
-reps = 0
+data = pandas.read_csv("data/english_words.csv")
+to_learn = data.to_dict(orient="records")
+
 
 # ---------UI SET UP---------------------#
 
@@ -19,41 +23,35 @@ window.config(padx=50, pady=50, bg=(BACKGROUND_COLOR))
 
 # ---------SHOW WORDS---------------------#
 
-
-# ---------TIMER MECHANISM---------------------#
-
-def start_timer(count):
-    global timer
-    global reps
-    time_session = SECONDS
-
-    if time_session > 0:
-        timer = window.after(10000, start_timer, count - 1)
-        canvas = Canvas(width=800, height=526, bg=(BACKGROUND_COLOR), highlightthickness=0)
-        flash_card_img_back = PhotoImage(file="images/card_back.png")
-        canvas.create_image(400, 280, image=flash_card_img_back)
-        canvas.create_text(400, 150, text="Russian", fill="black", font="Arial 40 italic")
-        canvas.grid(column=0, row=0, columnspan=2)
+def next_card():
+    current_card =random.choice(to_learn)
+    print(current_card["English"])
+    canvas.itemconfig(card_title, text="English")
+    canvas.itemconfig(card_word, text = current_card["English"])
 
 
 
 
-# ---------Buttonssss---------------------#
+
+
+
+
+
+# ---------Buttons---------------------#
 canvas = Canvas(width=800, height=526,bg=(BACKGROUND_COLOR), highlightthickness=0)
 flash_card_img_front = PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 280, image = flash_card_img_front)
-canvas.create_text(400, 150, text="English", fill = "black", font = "Arial 40 italic")
-
-
+canvas.create_image(400, 263, image = flash_card_img_front)
+card_title = canvas.create_text(400, 150, text="English", fill = "black", font = "Arial 40 italic")
+card_word = canvas.create_text(400, 263, text="word", fill = "black", font = "Arial 60 bold")
 canvas.grid(column=0, row=0, columnspan=2)
 
 right_image = PhotoImage(file="images/right.png")
-button_right = Button(image=right_image,highlightbackground=(BACKGROUND_COLOR))
+button_right = Button(image=right_image,highlightbackground=(BACKGROUND_COLOR), command=next_card)
 button_right.grid(column=1, row=1)
 
 wrong_image = PhotoImage(file="images/wrong.png")
-button_wrong = Button(image=wrong_image, highlightbackground=(BACKGROUND_COLOR))
+button_wrong = Button(image=wrong_image, highlightbackground=(BACKGROUND_COLOR), command=next_card)
 button_wrong.grid(column=0, row=1)
 
-start_timer(SECONDS)
+next_card()
 window.mainloop()
